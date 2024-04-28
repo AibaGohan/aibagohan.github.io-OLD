d05635c0294d822499717ca5a6f8948e262b45df
AibaGohan <serdrrrus@gmail.com>
2024-04-28 17:27:31 -0400

    1

---
+++ navigation.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger').addEventListener('click', function() {
+    const nav = document.getElementById('navbar');
+    nav.classList.toggle('active'); // 切换 'active' 类，控制显示和隐藏
+});

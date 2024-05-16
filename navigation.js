d97a02f9429bf778d7867ac1f224c0abfacd8064
AibaGohan <serdrrrus@gmail.com>
2024-05-16 06:30:58 -0400

    1

--- navigation.js
+++ navigation.js
@@ -1,7 +1,35 @@
 document.querySelector('.hamburger').addEventListener('click', function() {
     const nav = document.getElementById('navbar');
-    nav.classList.toggle('active'); // 切换 'active' 类，控制显示和隐藏
+    nav.classList.toggle('active');
+    document.body.classList.toggle('menu-open');
+
+    // 切换图标
+    this.classList.toggle('active');
+    if (this.classList.contains('active')) {
+        this.innerHTML = '&times;'; // X号
+    } else {
+        this.innerHTML = '&#9776;'; // 汉堡包
+    }
+
+    // 移动端显示联系方式
+    const contactInfo = document.createElement('div');
+    contactInfo.classList.add('contact-info');
+    contactInfo.innerHTML = `
+        <p style="font-size: 1.3rem; font-weight: 800; margin-bottom: 5px;">連絡先</p>
+        <p>騰訊QQ：1016070674</p>
+        <p>百度貼吧：@相葉五月半</p>
+        <p>Discord：@ShortIslandBoilingTea</p>
+    `;
+    if (nav.classList.contains('active')) {
+        nav.appendChild(contactInfo);
+    } else {
+        const existingContactInfo = document.querySelector('.contact-info');
+        if (existingContactInfo) {
+            existingContactInfo.remove();
+        }
+    }
 });
+
 
 document.getElementById('message-form').addEventListener('submit', function(event) {
     event.preventDefault();
